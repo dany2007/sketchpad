a92f84d46cce351a72ca99f96998b4aeda156594
Danut Mitrofan <dany200828@gmail.com>
2015-11-23 01:13:24 +0200

    I don't remember what I did :(

---
+++ js/javascript.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  for (var i = 1; i < 17; i++) {
+    for (var j = 1; j < 17; j++) {
+      $("#container").append($("<div/>")
+        .addClass("r" + j)
+        .addClass("c" + i)
+        .addClass("cell")
+        //.text(j)
+      );
+    }
+  }
+
+  $("div").hover(function() {
+    k = Math.floor(Math.random() * 3);
+    if (k < 1) {
+      $(this).css('background-color', 'red');
+    } else {
+      if (k < 2) {
+        $(this).css('background-color', 'purple');
+      } else $(this).css('background-color', 'yellow');
+    }
+  });
+
+
+});
+
+
+// $("div").hover(function(){
+// $(this).css('background-color','blue');
+// });
+//});
